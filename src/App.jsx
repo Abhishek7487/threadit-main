ea647f7dc35327909595899a33df7ca1b26f0b54
Abhishek7487 <dellabhishek7487@gmail.com>
2024-02-18 02:08:11 +0530

    Setting up Pages and Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyles.js";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 
-const StyledApp = styled.div``;
+import GlobalStyles from "./styles/GlobalStyles";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import Post from "./pages/Post";
+import Profile from "./pages/Profile";
+import CreatePost from "./pages/CreatePost";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <h1>Hello World!</h1>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route index element={<Navigate replace to="home" />} />
+          <Route path="home" element={<Homepage />} />
+          <Route path="/post" element={<Post />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-post" element={<CreatePost />} />
+
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,5 @@
+function CreatePost() {
+  return <div>Create post</div>;
+}
+
+export default CreatePost;
