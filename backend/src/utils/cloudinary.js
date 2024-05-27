948ae4d7998df5b798e2647a427ca5cf2a62cae3
Abhishek7487 <dellabhishek7487@gmail.com>
2024-05-27 17:29:50 +0530

    fix minor bug in cloudinary setup

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -2,9 +2,9 @@ import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
 cloudinary.config({
-  cloud_name: "dtzxzrfdw",
-  api_key: "271683496565384",
-  api_secret: "E--aGbyFY4RcHyVCfts0eNDu4dk",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 const uploadOnCloudinary = async (localFilePath) => {
